984d8a5b7ce73a40787f5f76009596b317f56f4f
Jeff Birkel <jbirkel@cox.net>
2013-07-11 18:35:59 +0000

    bugfix #1

--- web.js
+++ web.js
@@ -3,9 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buf =  fs.readFileSync( 'index.html' );
-  response.send( buf.toString() );
-  #response.send('Hello World 2!');
+  var buf = fs.readFileSync( 'index.html' );
+  var str = buf.toString();
+  response.send( str );
 });
 
 var port = process.env.PORT || 5000;
