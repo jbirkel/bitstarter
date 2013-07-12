c93d408b41b10826178bb65794897736e5c158b3
Jeff Birkel <jbirkel@cox.net>
2013-07-12 01:09:14 +0000

    part 3 final

--- grader.js
+++ grader.js
@@ -22,19 +22,31 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://infinite-waters-4964.herokuapp.com/";
+var FS_TEMP_FILE = "grader_temp_file";
 
+var argFile = false
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
+        console.log("File %s does not exist. Exiting.", instr);
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
+    argFile = true;
     return instr;
 };
+
+var argUrl = false;
+var assertUrlExists = function(url) {
+    var instr = url.toString();
+    argUrl = true;
+    return instr;
+}
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -61,14 +73,38 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL to web resource', clone(assertUrlExists), URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+
+    if (argFile || !argUrl) {
+	console.log('File: ' + program.file );
+	var checkJson = checkHtmlFile(program.file, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    }
+
+    else {
+	console.log('URL: ' + program.url );
+	var instr = program.url.toString();
+	rest.get(instr).on('complete', function(result) {
+            if (result instanceof Error) {
+		console.log("URL %s does not exist. Exiting.", instr);
+		process.exit(1);
+	    } else {
+		var checkJson = checkHtmlFile(FS_TEMP_FILE, program.checks);
+		var outJson = JSON.stringify(checkJson, null, 4);
+		console.log(outJson);
+	    }
+	});
+    }
+
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
